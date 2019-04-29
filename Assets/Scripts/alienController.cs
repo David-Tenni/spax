3093ee01a19c870464c0aed89b90506f3e7b6a2d
david <davidctenni@gmail.com>
2019-04-29 10:08:49 +1000

    added alien script

---
+++ Assets/Scripts/alienController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alienController : MonoBehaviour
+{
+    private bool goLeft;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (goLeft)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+        if (!goLeft)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "wall")
+        {
+            goLeft = !goLeft;
+        }
+    }
+
+
+}
